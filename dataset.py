2aa1644113175a28ed1f6a373f3fe729357ab6f6
japhletsandraj <japhletsandraj@gmail.com>
2026-01-24 18:06:50 +0530

    changed dataset

--- dataset.py
+++ dataset.py
@@ -2,5 +2,5 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import os
 os.environ["KAGGLEHUB_CACHE"] = "./dataset"
-path = kagglehub.dataset_download("ziya07/customer-churn-prediction-dataset")
+path = kagglehub.dataset_download("abdullah0a/telecom-customer-churn-insights-for-analysis")
 print(f"Data saved at: {path}")
